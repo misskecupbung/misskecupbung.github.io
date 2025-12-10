4d0884eda5541870ad1dd9be30f030c8c79f6c23
misskecupbung <ananda.dwirahmawati313@gmail.com>
2025-12-10 17:43:54 +0000

    Deploying to gh-pages from @ misskecupbung/misskecupbung.github.io@7420e250556ff9d3762289960d02bd0b65a2b7ec 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -215,13 +215,6 @@ ninja.data = [{
           window.open("https://www.linkedin.com/in/anandadwir", "_blank");
         },
       },{
-        id: 'social-rss',
-        title: 'RSS Feed',
-        section: 'Socials',
-        handler: () => {
-          window.open("/feed.xml", "_blank");
-        },
-      },{
         id: 'social-x',
         title: 'X',
         section: 'Socials',
